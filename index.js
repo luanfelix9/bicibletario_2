c65dc2e085031c1a83f7a331eff2783466aefb05
Luan Felix de Jesus <84149688+luanfelix9@users.noreply.github.com>
2022-06-08 18:50:33 -0300

    Primeiro Commit com postgres03

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ app.get("/", async (req,res)=>{
     const DB = require('./infra/postgresConnect');
     let produtos = await DB.query('select * from produtos').catch(err=>err);
     console.log(produtos)
-   return  res.send('Servidor ok'+produtos);
+
+
+
+   return  res.json(produtos);
 }); 
 
 app.listen(PORT, console.log('Servidor rodando na porta: ' + PORT ));
