e5aa13918b7b8fc73586ce179acd1a788bc5993d
Luan Felix de Jesus <84149688+luanfelix9@users.noreply.github.com>
2022-06-08 18:07:34 -0300

    Primeiro Commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", function(req,res){
+   return  res.send('Servidor ok');
+}); 
+
+app.listen(PORT, console.log('Servidor rodando na porta: ' + PORT ));
